fe31c0bc1f1d1f11655cdcc01a91b23f137e59e5
ParkSuhhyun <2022111511@dgu.ac.kr>
2024-06-18 16:55:34 +0900

    keyword table create

--- simbathonproject/main/models.py
+++ simbathonproject/main/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Varsity(models.Model):
+    college=models.CharField(max_length=20, null=True)
     major=models.CharField(max_length=20)
     like_count=models.PositiveIntegerField(default=0)
     image_front=models.ImageField(null=True)
     image_back=models.ImageField(null=True)
+
+class Keyword(models.Model):
+    keyword=models.CharField(max_length=20, null=True)
+    varsity_id=models.ForeignKey(Varsity, on_delete=models.CASCADE, related_name='keywords')
