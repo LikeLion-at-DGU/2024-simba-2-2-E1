d37f260525402cbcc417ed69023d4b9f4ac9a682
박성재 <sem201@naver.com>
2024-06-19 19:33:23 +0900

    feat 커스텀 과잠 페이지

--- simbathonproject/main/views.py
+++ simbathonproject/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def mainpage(request):
     return render(request,'main/mainpage.html')
+
+
+def custompage(request):
+    return render(request,'main/custompage.html')
