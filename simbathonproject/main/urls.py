936f3ce07a09246e2c05cbf9c836c7cb12a17190
박성재 <sem201@naver.com>
2024-06-20 12:44:03 +0900

    chore

--- simbathonproject/main/urls.py
+++ simbathonproject/main/urls.py
@@ -4,6 +4,7 @@ from main import views
 app_name='main'
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
-    path('custom/',views.custompage,name='custompage')
+    path('custom/',views.custompage,name='custompage'),
+    path('design/', views.designpage,name='design_start_page')
 ]
 
--- simbathonproject/main/views.py
+++ simbathonproject/main/views.py
@@ -5,3 +5,5 @@ def mainpage(request):
     return render(request,'main/mainpage.html')
 def custompage(request):
     return render(request,'main/custompage.html')
+def designpage(request):
+    return render(request,'design/select_page.html')
