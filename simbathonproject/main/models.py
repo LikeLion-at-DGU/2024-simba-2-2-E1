71384393ff26835ba0c3a5b9db8855405abbc006
ryann1203 <oxford1203@naver.com>
2024-06-20 14:06:12 +0900

    like함수 만드는중

--- simbathonproject/main/models.py
+++ simbathonproject/main/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Varsity(models.Model):
     college=models.CharField(max_length=20, null=True)
     major=models.CharField(max_length=20)
-    like_count=models.PositiveIntegerField(default=0)
     image_front=models.ImageField(upload_to='varsity/', null=True)
     image_back=models.ImageField(upload_to='varsity/', null=True)
+    like_count=models.PositiveIntegerField(default=0)
 
 class Keyword(models.Model):
     keyword=models.CharField(max_length=20, null=True)
